3247cba1c012f1199f7a09934fe269fa4704fbfa
Gabriel Georgiu <gabriel.georgiu@gmail.com>
2021-06-07 15:02:10 +0300

    - pacakge restructure

--- src/com/fatninja/aoc/w1/p2/Main.java
+++ src/com/fatninja/aoc/w1/p2/Main.java
@@ -1,4 +1,4 @@
-package com.fatninja.aoc.p1;
+package com.fatninja.aoc.w1.p2;
 
 import com.fatninja.aoc.utils.FileUtils;
 
@@ -6,21 +6,22 @@ import java.io.IOException;
 import java.util.List;
 
 public class Main {
-
     public static void main(String[] args) throws IOException {
         int sum = 2020;
         List<Integer> integers = FileUtils.readFromFile("src/resources/w1/p1/input.txt");
 
         int[] result = getResult(integers, sum);
 
-        System.out.println(result[0] * result[1]);
+        System.out.println(result[0] * result[1] * result[2]);
     }
 
     private static int[] getResult(List<Integer> integers, int sum) {
-        for (int i =0 ;i < integers.size(); i++) {
-            for (int j = i; j < integers.size(); j++) {
-                if (integers.get(i) + integers.get(j) == sum) {
-                    return new int[] {integers.get(i), integers.get(j)};
+        for (int i = 0; i < integers.size() - 3; i++) {
+            for (int j = i + 1; j < integers.size() - 2; j++) {
+                for (int k = j + 1; k < integers.size() - 1; k++) {
+                    if (integers.get(i) + integers.get(j) + integers.get(k) == sum) {
+                        return new int[]{integers.get(i), integers.get(j), integers.get(k)};
+                    }
                 }
             }
         }
