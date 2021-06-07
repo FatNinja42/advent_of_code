9c24014e70f05f6ea7c154ce18367cb29b309060
Gabriel Georgiu <gabriel.georgiu@gmail.com>
2021-06-07 19:29:53 +0300

    aoc_2020_d3 	- add p1 	- add p2

---
+++ src/com/fatninja/aoc/utils/MatrixUtils.java
@@ -0,0 +1,35 @@
+package com.fatninja.aoc.utils;
+
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
+
+public class MatrixUtils {
+
+    public static char[][] readCharMatrixFromFile(String path) throws IOException {
+        List<String> lines = Files.lines(Paths.get(path))
+                .collect(toList());
+
+        char [][] matrix = new char[lines.size()][lines.get(0).length()];
+        int i = 0;
+        for (String line : lines) {
+            char[] chars = line.toCharArray();
+            matrix[i++] = chars;
+        }
+
+        return matrix;
+    }
+
+    public static void printCharMatrix(char[][] matrix) {
+        for (char[] aMatrix : matrix) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                System.out.print(aMatrix[j]);
+            }
+            System.out.print("\n");
+        }
+    }
+}
