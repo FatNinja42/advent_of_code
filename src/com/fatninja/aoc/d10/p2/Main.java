0e7760cd60a33ee6bf36a35a520616ffbeef8849
Gabriel Georgiu <gabriel.georgiu@nagarro.com>
2021-09-11 18:54:53 +0200

    aoc2020_d10 -add d10p1 and d10p2

---
+++ src/com/fatninja/aoc/d10/p2/Main.java
@@ -0,0 +1,33 @@
+package com.fatninja.aoc.d10.p2;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static com.fatninja.aoc.utils.FileUtils.readIntegerListFromFile;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        List<Integer> adapters = readIntegerListFromFile("src/resources/d10/input.txt");
+        adapters.add(0);
+        List<Integer> sortedAdapters = adapters.stream()
+                .sorted()
+                .collect(Collectors.toList());
+
+        int maxAdapter = sortedAdapters.get(sortedAdapters.size() - 1) + 3;
+        sortedAdapters.add(maxAdapter);
+
+        long[] permutations = new long[maxAdapter + 1];
+        permutations[0] = 1;
+
+        for (Integer adapter : sortedAdapters) {
+            for (int i = 1; i <= 3; i++) {
+                if (sortedAdapters.contains(adapter + i)) {
+                    permutations[adapter + i] += permutations[adapter];
+                }
+            }
+        }
+
+        System.out.println(permutations[maxAdapter]);
+    }
+}
