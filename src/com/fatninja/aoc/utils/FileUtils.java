d05c50d0b86091093e2a8a9b57dfdc64310024d3
Gabriel Georgiu <gabriel.georgiu@nagarro.com>
2021-09-19 19:09:43 +0200

    aoc_2020_d15 add d15

--- src/com/fatninja/aoc/utils/FileUtils.java
+++ src/com/fatninja/aoc/utils/FileUtils.java
@@ -3,6 +3,7 @@ package com.fatninja.aoc.utils;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -25,5 +26,17 @@ public class FileUtils {
 
     public static List<String> readStringListFromFile(String path) throws IOException {
         return Files.lines(Paths.get(path)).collect(Collectors.toList());
+    }
+
+    public static List<Integer> readCommaSeparatedIntListFromFile(String path) throws IOException {
+        List<String> lines = Files.lines(Paths.get(path))
+                .map(String::new)
+                .collect(Collectors.toList());
+
+        String[] numbers = lines.get(0).split(",");
+
+        return Arrays.stream(numbers)
+                .map(Integer::valueOf)
+                .collect(Collectors.toList());
     }
 }
