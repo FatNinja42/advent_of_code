346e4b0f3e974a4007ea9a5acbe549ae19229351
Gabriel Georgiu <gabriel.georgiu@nagarro.com>
2021-09-19 19:44:15 +0200

    add d15p2 comment

--- src/com/fatninja/aoc/d15/p2/Main.java
+++ src/com/fatninja/aoc/d15/p2/Main.java
@@ -9,6 +9,10 @@ public class Main {
 
     private static final int P2_LIMIT = 30_000_000;
 
+    /*
+     * The implementation made for part 1 also works for part 2, but being implemented using hash tables
+     * the execution time is way slower than this one.
+     */
     public static void main(String[] args) throws IOException {
         List<Integer> integers = FileUtils.readCommaSeparatedIntListFromFile("src/resources/d15/input.txt");
         int[] occurrences = new int[100000000];
